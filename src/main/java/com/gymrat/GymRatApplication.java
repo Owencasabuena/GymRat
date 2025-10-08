6bc9f10930ef6dbb180ffb72e0939d06c37d558d
Owen <owencasabuena@gmail.com>
2025-10-08 19:59:14 +0800

    Initial Spring Boot project setup

---
+++ src/main/java/com/gymrat/GymRatApplication.java
@@ -0,0 +1,13 @@
+package com.gymrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymRatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymRatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymrat/GymRatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymRatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
